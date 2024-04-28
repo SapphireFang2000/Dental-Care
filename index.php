6be061943bd10a6cad2b519da5a5e6a404ca3b2d
SapphireFang2000 <n.a.refat2000@gmail.com>
2024-04-29 04:08:24 +0600

    Final

---
+++ index.php
@@ -0,0 +1,341 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','contact_db') or die('connection failed');
+
+if(isset($_POST['submit'])){
+
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $number = $_POST['number'];
+   $date = $_POST['date'];
+
+   $insert = mysqli_query($conn, "INSERT INTO `contact_form`(name, email, number, date) VALUES('$name','$email','$number','$date')") or die('query failed');
+
+   if($insert){
+      $message[] = 'appointment made successfully!';
+   }else{
+      $message[] = 'appointment failed';
+   }
+
+}
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Complete Responsive Dentist Website Design Tutorial</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- bootstrap cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<header class="header fixed-top">
+
+   <div class="container">
+
+      <div class="row align-items-center justify-content-between">
+
+         <a href="#home" class="logo">dental<span>Care.</span></a>
+
+         <nav class="nav">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#services">services</a>
+            <a href="#reviews">reviews</a>
+            <a href="#footer">contact</a>
+         </nav>
+
+         <a href="#contact" class="link-btn">make appointment</a>
+
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </div>
+
+   </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+   <div class="container">
+
+      <div class="row min-vh-100 align-items-center">
+         <div class="content text-center text-md-left">
+            <h3>let us brighten your smile</h3>
+            <p>Welcome to DentalCare, where you’ll find high-quality dental care for your whole family.</p>
+            <a href="#contact" class="link-btn">make appointment</a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+   <div class="container">
+
+      <div class="row align-items-center">
+
+         <div class="col-md-6 image">
+            <img src="images/about-img.jpg" class="w-100 mb-5 mb-md-0" alt="">
+         </div>
+
+         <div class="col-md-6 content">
+            <span>about us</span>
+            <h3>True Healthcare For Your Family</h3>
+            <p>All the care your family needs — all under one roof. From your infant’s first dental exam to teeth whitening before your wedding day, you’ll be glad DentalCare is where you chose to call home for your whole family’s dental health needs.<br><br>Your dentist will work closely with you to create the treatment plan that addresses your concerns and prepares you for a lifetime of smiles.</p>
+            <a href="#contact" class="link-btn">make appointment</a>
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading">our services</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/icon-1.svg" alt="">
+         <h3>Alignment specialist</h3>
+         <p>Whether your teeth are crooked, crowded, or separated by gaps, Our Doctor is there for you</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.svg" alt="">
+         <h3>Cosmetic dentistry</h3>
+         <p>We focus on improving the appearance of your smile.Treatments including teeth whitening, dental bonding and veneers</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.svg" alt="">
+         <h3>Oral hygiene experts</h3>
+         <p>ood oral health helps you enjoy life. It lets you: speak clearly; taste, chew, and swallow delicious and nutritious foods; and show your feelings</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.svg" alt="">
+         <h3>Root canal specialist</h3>
+         <p>Highly trained endodontists (dental specialists) repair tissues inside your tooth.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.svg" alt="">
+         <h3>Live dental advisory</h3>
+         <p>This allows people to receive advice on dental emergencies, cosmetic concerns and other urgent matters immediately,</p>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.svg" alt="">
+         <h3>Cavity inspection</h3>
+         <p> We can usually detect your tooth decay by: Asking about tooth pain and sensitivity; Examining your mouth and teeth;</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- process section starts  -->
+
+<section class="process">
+
+   <h1 class="heading">work process</h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/process-1.png" alt="">
+         <h3>Cosmetic Dentistry</h3>
+         <p>If you’re not satisfied with your smile, modern cosmetic dentistry can help.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/process-2.png" alt="">
+         <h3>Pediatric Dentistry</h3>
+         <p>specialize in diagnosing and treating dental problems in infants, children, and teenagers.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/process-3.png" alt="">
+         <h3>Dental Implants</h3>
+         <p> We are surgically placed in your jawbone, where they serve as the roots of missing teeth. </p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- process section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews" id="reviews">
+
+   <h1 class="heading"> satisfied clients </h1>
+
+   <div class="box-container container">
+
+      <div class="box">
+         <img src="images/preotee.jpg" alt="preotee">
+         <p>Excellent doctor!!!Very thorough and caring. I'm terribly afraid of the dentist and she's very sensitive to that and goes the extra mile to make you feel comfortable. Love her!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Faika Fairuj Preotee</h3>
+         <span>satisfied client</span>
+      </div>
+
+      <div class="box">
+         <img src="images/shuvo.jpg" alt="">
+         <p>No pain or problems with my extractions and she goes above and beyond the call of duty to make sure you understand what you should do with your teeth and mouth so you do not have future health risks. I found her to be very professional and truly caring about your oral health</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Shuvashis Sarker</h3>
+         <span>satisfied client</span>
+      </div>
+
+      <div class="box">
+         <img src="images/refat.jpg" alt="">
+         <p>Doctor was very caring and understanding towards my children during our visit. My children are very scared of dentists to begin with. But she took the time to talk to them through the procedures and calm them down. She never talked down to the kids - neither did she apply pressure if they were not ready for the procedure.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Shamim Rahim</h3>
+         <span>satisfied client</span>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+   <h1 class="heading">make appointment</h1>
+
+   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <?php 
+         if(isset($message)){
+            foreach($message as $message){
+               echo '<p class="message">'.$message.'</p>';
+            }
+         }
+      ?>
+      <span>your name :</span>
+      <input type="text" name="name" placeholder="enter your name" class="box" required>
+      <span>your email :</span>
+      <input type="email" name="email" placeholder="enter your email" class="box" required>
+      <span>your number :</span>
+      <input type="number" name="number" placeholder="enter your number" class="box" required>
+      <span>appointment date :</span>
+      <input type="datetime-local" name="date" class="box" required>
+      <input type="submit" value="make appointment" name="submit" class="link-btn">
+   </form>  
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer" id="footer">
+
+   <div class="box-container container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>phone number</h3>
+         <p>+8801819191651</p>
+         <p>+8807114114122</p>
+      </div>
+      
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>our address</h3>
+         <p>Love Road, Tejgoan,Dhaka</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-clock"></i>
+         <h3>opening hours</h3>
+         <p>00:07am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>email address</h3>
+         <p>shuvashis.cse.200104116@aust.edu</p>
+         <p>shamim.cse.200104125@aust.edu</p>
+      </div>
+
+   </div>
+
+   <div class="credit"> This Project was built by <span>Shuvashis Sarker and Shamim Rahim</span>  </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
